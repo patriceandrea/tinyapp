43676e2e48054c38631b50dcf5e56df722b4dcbe
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-23 21:33:57 +0000

    adds login and register features

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const { request } = require("express");
+const { render } = require("ejs");
 
 const app = express();
 const PORT = 8080; // default port 8080
@@ -12,6 +13,15 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const findEmail = (users, email) => {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return false;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -48,8 +58,7 @@ app.get("/urls", (req, res) => {
   const urls = urlDatabase;
   const templateVars = {
     urls,
-    username: req.cookies ? req.cookies["username"] : null
-
+    user: users[req.cookies["user_id"]]
   }
   res.render("urls_index", templateVars);
 });
@@ -62,19 +71,21 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies ? req.cookies["username"] : null };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 //showpage
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls/new/:id", (req, res) => {
   res.render("urls_new");
 });
+
+
 
 // generates a new shorturl to the longurl 
 app.post("/urls", (req, res) => {
@@ -88,6 +99,7 @@ app.post("/urls", (req, res) => {
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 }
+
 
 
 
@@ -120,10 +132,81 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
-// login username
+// login 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // lookup email in user Object 
+  const findUser = findEmail(users, email);
+  console.log(findUser);
+  if (!findUser) {
+    return res.status(403).send('Email cannot be found');
+  }
+
+  if (password !== findUser.password) {
+    return res.status(403).send('Password do not match');
+  }
+
+  res.cookie('user_id', findUser.id);
   res.redirect('/urls');
 });
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+
+
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email === "") {
+    return res.status(400).send('Email not written');
+  } else if (password === '') {
+    return res.status(400).send('Password not written');
+  }
+  const emailLookup = findEmail(users, email)
+  if (emailLookup) {
+    return res.status(400).send('Email already exist');
+  }
+
+  const id = generateRandomString();
+  users[id] = { id, email, password };
+  res.cookie('user_id', id);
+  return res.redirect('/urls');
+
+});
+
+
+// render to registration page 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]]
+  };
+
+  res.render("urls_register", templateVars);
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+})
+
+app.get('/login', (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_login", templateVars);
+})
